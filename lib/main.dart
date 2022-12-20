71ed9674e0fb0645061be2eadc6516763e1d35fb
IagoAntunes <iagoantunes.f@gmail.com>
2022-12-20 11:51:34 -0300

    Creating Provider / Store

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro/pages/pomodoro.dart';
+import 'package:pomodoro/store/pomodore.store.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider<PomodoroStore>(
+          create: (context) => PomodoroStore(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Pomodoro(),
       ),
-      home: const Pomodoro(),
     );
   }
 }
--- lib/pages/pomodoro.dart
+++ lib/pages/pomodoro.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro/components/cronometro.dart';
 import 'package:pomodoro/components/entrada_tempo.dart';
+import 'package:pomodoro/store/pomodore.store.dart';
+import 'package:provider/provider.dart';
 
 class Pomodoro extends StatelessWidget {
   const Pomodoro({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -17,8 +20,8 @@ class Pomodoro extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                EntradaTempo(value: 25, title: "Trabalho"),
-                EntradaTempo(value: 5, title: "Descanso"),
+                EntradaTempo(value: store.tempoTrabalho, title: "Trabalho"),
+                EntradaTempo(value: store.tempoDescanso, title: "Descanso"),
               ],
             ),
           ),
